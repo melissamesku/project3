9e9bee8362838239380c27e85f26524057deb5de
Amanda Corlett <amanda.corlett@gmail.com>
2015-11-21 16:47:39 -0500

    init commit user schema

---
+++ models/user.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose');
+// var ComplaintSchema = require('./complaint').schema;
+
+var userSchema = new mongoose.Schema({
+	username: { type: String, required: true, unique: true };
+	email: { type: String, required: true, unique: true },
+	password_hash: String,
+	age: Number,
+	location: String,
+	time_capsule: [capsuleSchema],
+	created_at: Date
+});
+
+var User = mongoose.model('User', userSchema);
+module.exports = User;
