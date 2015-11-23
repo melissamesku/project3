bf50b2507fc5e3c3816d002b43d63814ecb3ac2f
ericseandevlin <ericdevlin@me.com>
2015-11-23 08:08:52 -0500

    signup working, still need login

--- models/user.js
+++ models/user.js
@@ -2,12 +2,12 @@ var mongoose = require('mongoose');
 var capsuleSchema = require('./capsule').schema;
 
 var userSchema = new mongoose.Schema({
-	username: { type: String, required: true, unique: true };
-	email: { type: String, required: true, unique: true },
+	username: { type: String, required: true, unique: true },
+	// email: { type: String, required: true, unique: true },
 	password_hash: String,
-	age: Number,
-	location: String,
-	time_capsules: [capsuleSchema],
+	// age: Number,
+	// location: String,
+	// time_capsules: [capsuleSchema],
 	created_at: { type:Date, default: Date.now }
 });
 
--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,112 @@
 $(function() {
 console.log('js loaded');
 
-// SIGN-UP -----------------------
+// SIGN-UP BUTTON
 $('#sign-up').click(function(){
   console.log('clicked sign-up');
   signUpForm();
-  $('#sign-up').hide();
+  // $('#sign-up').hide();
 });
 
-}; // end doc ready
+// LOG-IN BUTTON
+$('#log-in').click(function(){
+  console.log('clicked log-in');
+  loginForm();
+  // $('#log-in').hide();
+});
+
+}); // end doc ready
+
+// GLOBAL VARIABLES --------------
+var user = null;
+var formContainer = $('#form-container');
+
+// SIGN-UP -----------------------
+var signUpForm = function() {
+		console.log('showing sign up form');
+		// formContainer.show();
+
+		var template = Handlebars.compile($('#signup-template').html());
+		formContainer.append(template);
+
+    // REGISTER BUTTON
+		$('#register-button').click(function(){
+			console.log('clicked register');
+			newUser();
+			// $('#sign-up').hide();
+		});
+	}; // sign up form
+
+
+var newUser = function() {
+	user = {
+		username: $('#username').val(),
+		password: $('#password').val(),
+    // email: 'test@test.com',
+    // age: 20,
+    // location: 'New York'
+	};
+	console.log(user.username+" created app side");
+
+	$.ajax({
+		url: "http://localhost:3000/users",
+		method: "POST",
+    dataType: 'json',
+		data: user
+	}).done(function(data){
+    console.log("sent sign-up info to server");
+    console.log(data.username+" signup successful");
+
+    // get main page form goes here
+    formContainer.empty();
+    console.log(data);
+    formContainer.append("<p> name: "+data.username+"</p>");
+	});
+
+	user = Cookies.get("loggedinId");
+}; // end newUser
+// END SIGN-UP -----------------------
+
+// LOGIN ---------------------
+var loginForm = function() {
+	console.log('showing login form');
+	// formContainer.empty();
+
+	var template = Handlebars.compile($('#login-template').html());
+	formContainer.append(template);
+
+  // LOGIN BUTTON - the one on the actual form
+	$('#login-button').click(function(){
+		console.log('clicked login-button');
+		loginPost();
+	});
+}; // sign up form
+
+
+var loginPost = function() {
+	user = {
+		username: $('#username').val(),
+		password: $('#password').val(),
+	};
+
+	$.ajax({
+		url: 'http://localhost:3000/login',
+		method: "POST",
+		dataType: 'json',
+		data: user
+	}).done(function(data) {
+		console.log("sent login info to server");
+		console.log(data.username+" login successful");
+
+		user = Cookies.get("loggedinId");
+
+    // get main page form goes here
+    // formContainer.empty();
+    formContainer.append("<p>"+data+"</p>");
+
+	}).fail(function(){
+		var template = Handlebars.compile($('#status-template').html());
+		formContainer.append(template("try again"));
+	}); // end fail
+}; //end loginPost
+// END LOGIN ---------------------
