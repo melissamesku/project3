fb9948fc6cf01b9009355c138e10822730874dd7
ericseandevlin <ericdevlin@me.com>
2015-11-30 15:39:41 -0500

    added rough email

--- public/js/email.js
+++ public/js/email.js
@@ -1,29 +1,33 @@
 
 
-  // Create a function to log the response from the Mandrill API
-  function log(obj) {
-      $('#response').text(JSON.stringify(obj));
+// Create a function to log the response from the Mandrill API
+function log(obj) {
+    $('#response').text(JSON.stringify(obj));
+}
+
+// create a new instance of the Mandrill class with your API key
+var m = new mandrill.Mandrill('API KEY');
+
+// create a variable for the API call parameters
+var params = {
+  "message": {
+    "from_email":"ericdevin@me.com",
+    "to":[{"email":"ericdevlin@me.com"}],
+    "subject": "Hello from the past!",
+    "html": "Here are your capsules!!"
   }
+};
 
-  // create a new instance of the Mandrill class with your API key
-  var m = new mandrill.Mandrill('ErC_Pp1x5G3LBsNSbDgQLw');
+function sendTheMail() {
+console.log("sending email");
+  m.messages.send(params, function(res) {
+      log(res);
+  }, function(err) {
+      log(err);
+  });
+}
 
-  // create a variable for the API call parameters
-  var params = {
-      "message": {
-          "from_email":"ericdevin@me.com",
-          "to":[{"email":"ericdevlin@me.com"}],
-          "subject": "Sending a text email from the Mandrill API",
-          "html": "I'm learning the Mandrill API."
-      }
-  };
-
-  function sendTheMail() {
-  // Send the email!
-
-      m.messages.send(params, function(res) {
-          log(res);
-      }, function(err) {
-          log(err);
-      });
-  }
+$('#email-button').click(function(){
+  console.log('clicked email button');
+  sendTheMail();
+});
