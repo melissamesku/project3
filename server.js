dc9ee2251040f7369c081f5a8e09c0f295b968db
Melissa Mesku <mmesku@gmail.com>
2015-11-30 16:51:49 -0500

    console loggin mongoUri

--- server.js
+++ server.js
@@ -22,6 +22,11 @@ app.use(cookieParser());
 
 // DATABASE FOR LOCAL OR HEROKU DEPLOYMENT
 var mongoUri =  process.env.MONGOLAB_URI || 'mongodb://localhost/timecapsule_app';
+console.log('----------------------------------')
+console.log('----------------------------------')
+console.log(mongoUri)
+console.log('----------------------------------')
+console.log('----------------------------------')
 mongoose.connect(mongoUri);
 
 // DATABASE SEED - RUN THIS THE FIRST TIME, THEN COMMENT IT OUT!
