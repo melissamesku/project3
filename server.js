1ea741e6279500aaaa3b5da9dadf0ed0f34ff82b
Melissa Mesku <mmesku@gmail.com>
2015-11-30 19:14:40 -0500

    commented out mandrill require

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var express      = require('express'),
     md5          = require('md5'),
     cookieParser = require('cookie-parser'),
     morgan       = require('morgan');
-    mandrill     = require('node-mandrill')('ErC_Pp1x5G3LBsNSbDgQLw');
+    // mandrill     = require('node-mandrill')('ErC_Pp1x5G3LBsNSbDgQLw');
     // var mandrill = require('mandrill-api/mandrill');
 
     // var mandrill_client = new mandrill.Mandrill('ErC_Pp1x5G3LBsNSbDgQLw');
